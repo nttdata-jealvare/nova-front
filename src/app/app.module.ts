69f154720bc108627fb1bde0c8fc815cde4efd42
nttdata-jealvare <jesus.alvarezcoll@everis.nttdata.com>
2022-04-19 15:09:42 +0200

    Implementado el nuevo icono de basura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
@@ -21,6 +27,11 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     MatSelectModule,
     NoopAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,14 +1,14 @@
 import { Task } from './task';
 
 export const TASKS: Task[] = [
-  { id: 11, description: 'Task 11', status: 'In Progress'},
-  { id: 12, description: 'Task 12', status: 'In Progress'},
-  { id: 13, description: 'Task 13', status: 'Pending'},
-  { id: 14, description: 'Task 14 Task 14 Task 14 Task 14 Task 14 Task 14 Task 14 Task 14 Task 14', status: 'New'},
-  { id: 15, description: 'Task 15', status: 'In Progress'},
-  { id: 16, description: 'Task 16', status: 'Completed'},
-  { id: 17, description: 'Task 17', status: 'Completed'},
-  { id: 18, description: 'Task 18', status: 'Pending'},
-  { id: 19, description: 'Task 19', status: 'New'},
+  { id: 11, description: 'Test sobre la tarea SCRUMTSK00023', status: 'In Progress'},
+  { id: 12, description: 'Comunicación con el cliente sobre la nueva implementación', status: 'In Progress'},
+  { id: 13, description: 'Subida a producción de la tarea SCRUMTSK00223', status: 'Pending'},
+  { id: 14, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non arcu nec mi aliquam dapibus vitae vitae lectus. Ut ornare ultricies tellus, vel accumsan mauris eleifend ac. Fusce consequat egestas rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi condimentum, libero ac facilisis euismod', status: 'New'},
+  { id: 15, description: 'Actualización de seguridad 502_L12', status: 'In Progress'},
+  { id: 16, description: 'Implementación DevOps sobre nuevo elemento Azure', status: 'Completed'},
+  { id: 17, description: 'Continuación Test Unitarios', status: 'Completed'},
+  { id: 18, description: 'Renovación de credenciales cuenta Kubernetes', status: 'Pending'},
+  { id: 19, description: 'Actualización de CRM por vulnerabilidad 1002:122a', status: 'New'},
   { id: 20, description: 'Task 20', status: 'In Progress'}
 ];
